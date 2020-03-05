12b7f63322708b06282a4dcf9fcfb40cc1d5be08
王二飞 <459876292@qq.com>
2020-03-05 16:19:18 +0800

    去掉无用的生命周期函数

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -6,28 +6,6 @@ import markdown from "./markdown";
 
 export default class Index extends Component {
 
-  componentWillMount() {
-  }
-
-  componentDidMount() {
-  }
-
-  componentWillUnmount() {
-  }
-
-  componentDidShow() {
-  }
-
-  componentDidHide() {
-  }
-
-  /**
-   * 指定config的类型声明为: Taro.Config
-   *
-   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
-   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
-   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
-   */
   config: Config = {
     navigationBarTitleText: '首页'
   }
